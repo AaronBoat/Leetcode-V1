6d6c45b1e8b6241aa55d42e90b28462a8d8d2898
Aaron <2360679476@qq.com>
2026-08-10 15:51:03 +0800

    roll back to v4

--- 53.cpp
+++ 53.cpp
@@ -1,38 +1,29 @@
 class Solution
 {
-private:
-    void PrintLog(int i)
-    {
-    }
-
 public:
     int maxSubArray(vector<int> &nums)
     {
-        if (nums.size() < 1)
+        if(nums.size() < 1)
         {
             return 0;
         }
-        if (nums.size() == 1)
+        if(nums.size() == 1)
         {
             return nums[0];
         }
         assert(nums.size() > 1);
         int max_sum = nums[0];
-        int min_sum = min(nums[0], 0);
-        int max_diff = max_sum - min_sum;
+        int min_sum = min(nums[0] , 0);
+        int max_diff = -2e5;
         long long pre_sum = 0;
         // vector<int> preSum(nums.size());
         for (int i = 0; i < nums.size(); i++)
         {
             pre_sum += nums[i];
             max_sum = max_sum >= pre_sum ? max_sum : pre_sum;
-            max_diff = std::max(max_diff, max_sum - min_sum);
             min_sum = min_sum <= pre_sum ? min_sum : pre_sum;
-            //
-            cout << "now at round " << i << " ";
-            cout << "max: "<<max_sum << " min: "<<min_sum<<" diff: "<<max_diff<<endl;
-            ///
+            pre_sum = max(pre_sum , max_sum - min_sum);
         }
-        return max_diff;
+        return max_sum - min_sum;
     }
 };
