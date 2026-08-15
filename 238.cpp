afbbf93a3df07645a1c35f23641ec7782fea77db
Aaron <2360679476@qq.com>
2026-08-15 10:43:04 +0800

    v2

--- 238.cpp
+++ 238.cpp
@@ -9,30 +9,32 @@ public:
         pre_pro[0] = nums[0];
         for (int i = 1; i < nums.size(); i++)
         {
-            pre_pro[i] = pro_pro[i - 1] * nums[i];
+            pre_pro[i] = pre_pro[i - 1] * nums[i];
         }
 
-        int back = nums.size() - 1 suf_pro[back] = nums[back];
+        int back = nums.size() - 1;
+        suf_pro[back] = nums[back];
         for (int i = back - 1; i >= 0; i--)
         {
             suf_pro[i] = suf_pro[i + 1] * nums[i];
         }
 
-        vector ans(nums.size());
-        for(int i = 0 ; i < nums.size() ; i++)
+        vector<int> ans(nums.size());
+        for (int i = 0; i < nums.size(); i++)
         {
             int left_pro = 1;
-            if(i >= 1)
+            if (i >= 1)
             {
                 left_pro = pre_pro[i - 1];
             }
             int right_pro = 1;
-            if(i <= back - 1)
+            if (i <= back - 1)
             {
                 right_pro = suf_pro[i + 1];
             }
 
             ans[i] = left_pro * right_pro;
         }
+        return ans;
     }
 };
