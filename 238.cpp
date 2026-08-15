800106c62e2d207fedc02d3139170bba4c7407fc
Aaron <2360679476@qq.com>
2026-08-15 10:55:10 +0800

    v4

--- 238.cpp
+++ 238.cpp
@@ -3,10 +3,10 @@ class Solution
 public:
     vector<int> productExceptSelf(vector<int> &nums)
     {
-        vector<int> pre_pro(nums.size());
+        // vector<int> pre_pro(nums.size());
         vector<int> suf_pro(nums.size());
 
-        pre_pro[0] = nums[0];
+        // pre_pro[0] = nums[0];
         // for (int i = 1; i < nums.size(); i++)
         // {
         //     pre_pro[i] = pre_pro[i - 1] * nums[i];
@@ -19,22 +19,26 @@ public:
             suf_pro[i] = suf_pro[i + 1] * nums[i];
         }
 
-        vector<int> ans(nums.size());
+        // vector<int> ans(nums.size());
         int left_pro = 1;
 
         for (int i = 0; i < nums.size(); i++)
         {
 
-            int right_pro = 1;
+            // int right_pro = 1;
+            // if (i <= back - 1)
+            // {
+            //     right_pro = suf_pro[i + 1];
+            // }
             if (i <= back - 1)
+                suf_pro[i] = left_pro * suf_pro[i + 1];
+            else
             {
-                right_pro = suf_pro[i + 1];
+                suf_pro[i] = left_pro;
             }
-
-            ans[i] = left_pro * right_pro;
 
             left_pro *= nums[i];
         }
-        return ans;
+        return suf_pro;
     }
 };
