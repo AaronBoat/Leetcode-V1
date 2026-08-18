1826b6bd5cb6cfb172c7b8adb7c5746a1bd5f4b9
Aaron <2360679476@qq.com>
2026-08-18 10:55:15 +0800

    v1

---
+++ 141.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *p = head;
+        ListNode *q = head;
+        while(p != nullptr && q != nullptr)
+        {
+            if(p == q)
+            {
+                return true;
+            }
+            p = p->next;
+
+            q=q->next->next;
+        }
+        return false;
+    }
+};
