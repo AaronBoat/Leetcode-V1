571c34b6381d852fe13640965bb9912f8e2aff47
Aaron <2360679476@qq.com>
2026-08-18 10:04:27 +0800

    v2

--- 108.cpp
+++ 108.cpp
@@ -1,4 +1,4 @@
-#include<vector>
+#include <vector>
 using namespace std;
 
 /**
@@ -12,27 +12,30 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 private:
-    inline vector<int> sub_vector(vector<int>& vec,const int& begin,const int& end)
+    inline vector<int> sub_vector(const vector<int> &vec, const int &begin, const int &end)
     {
         assert(end >= begin);
         vector<int> sub_v(end - begin);
-        for(int delta = 0 ; delta < end - begin ; delta ++)
+        for (int delta = 0; delta < end - begin; delta++)
         {
             sub_v[delta] = vec[begin + delta];
         }
         return sub_v;
     }
+
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if(nums.size() == 0)
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        if (nums.size() == 0)
         {
             return nullptr;
         }
         const int mid_index = nums.size() / 2;
-        TreeNode* root(nums[mid_index]);
-        root->left = sortedArrayToBST(sub_vector(nums,0,mid_index));
+        TreeNode *root = new TreeNode(nums[mid_index]);
+        root->left = sortedArrayToBST(sub_vector(nums, 0, mid_index));
         root->right = sortedArrayToBST(sub_vector(nums,mid_index+1,nums.size()-1);
         return root;
     }
