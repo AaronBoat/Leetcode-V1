0e9a62c7d503fe3d5c32d93860da2c2777ae5467
Aaron <2360679476@qq.com>
2026-08-20 16:01:05 +0800

    v1

---
+++ 19.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head == nullptr)
+        {
+            return nullptr;
+        }
+        ListNode s = head;
+        ListNode f = head;
+        for(int i = 0 ; i < n + 1; i++)
+        {
+            f = f->next;
+            if(f == nullptr)
+            {
+                return nullptr;
+            }
+        }
+        while( f != nullptr)
+        {
+            f= f->next;
+            s=s->next;
+        }//s = n+1 个 倒数
+        assert(s->next != nullptr);
+        ListNode* s_n_n = s->next->next;
+        delete(s->next);
+        s->next = s_n_n;
+        return head;
+    }
+};
