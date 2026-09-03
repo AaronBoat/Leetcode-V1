3d6584a6b104d0eb86e7c34760d5c13a85b0a718
Aaron <2360679476@qq.com>
2026-09-03 16:09:55 +0800

    v2

--- 124.cpp
+++ 124.cpp
@@ -24,7 +24,10 @@ private:
         const int val = root->val;
         const int left_sub_path_max = max_path(root->left);
         const int right_sub_path_max = max_path(root->right);
-        ans = max(ans, left_sub_path_max + right_sub_path_max);
+        ans = max(ans, max(left_sub_path_max + right_sub_path_max + val,val));
+        //1
+//cout << " at pos: "<<val<<"with left and right: "<<left_sub_path_max<<" , "<<right_sub_path_max<<'\n';
+        //2
         return max(val,max(val+left_sub_path_max,val+right_sub_path_max));
     }
 
