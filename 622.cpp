3192948d21c18ca4f53f31c50d928eaef24d5173
Aaron <2360679476@qq.com>
2026-09-07 16:02:50 +0800

    v5

--- 622.cpp
+++ 622.cpp
@@ -47,7 +47,7 @@ public:
     bool enQueue(int value)
     {
 
-        if (tail_next != head)
+        if (to_minus_1(tail_next) != head)
         {
             q[tail_next] = value;
             // 1
